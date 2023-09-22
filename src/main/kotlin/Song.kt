10415a3286cd5a9d86111cf8638f5f6c338fe0f3
Serhat <me.serhaterdem@gmail.com>
2023-09-22 19:38:23 +0300

    How to crate a class & its properties, funs

---
+++ src/main/kotlin/Song.kt
@@ -0,0 +1,10 @@
+/**
+ * @param title The title of the song
+ * @param artist: the author of the song
+ * @param size : how many MB song is.
+ */
+class Song(val title:String, val artist:String, val size:Int) {
+    fun play(){
+        println("$title is playing by $artist")
+    }
+}
