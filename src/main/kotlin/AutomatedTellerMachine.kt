bc6a5d025fa49afcb4785e2e2a8cf2fd302e54c9
Serhat <me.serhaterdem@gmail.com>
2023-09-24 01:48:18 +0300

    Automated Teller Machine Project- Final

--- src/main/kotlin/AutomatedTellerMachine.kt
+++ src/main/kotlin/AutomatedTellerMachine.kt
@@ -1,4 +1,26 @@
-fun main(args:Array<String>) {
+import java.util.Scanner
 
+fun main(args:Array<String>) {
+    val scanner:Scanner=Scanner(System.`in`)
+    println("Please register...")
+
+    print("Name:")
+    val name:String=scanner.next()
+
+    print("Surname")
+    val surname:String=scanner.next()
+
+    print("Age:")
+    val age:Int=scanner.nextInt()
+
+    print("ID:")
+    val id:Int=scanner.nextInt()
+
+    print("Credit:")
+    val credit:Int=scanner.nextInt()
+
+
+    val user:User=User(name,surname,age,id,credit)
+    user.showMenu()
 
 }
--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,9 +1,61 @@
+import java.lang.Exception
 import java.util.*
 
-class User(name_param:String, salary_param:Int) {
-    var name=name_param
-        get() { return name.length.toString() }
-        set(value) {
-            field = if (name.length<3) "" else value
+class User(name: String, surname: String, age: Int, id: Int, private var credit: Int) {
+    init {
+        credit=0
+    }
+    private var name=name
+        set(value){
+            field=if (value.isNotEmpty()) name else throw Exception("Please enter a valid name")
         }
+        get() {
+            return field.uppercase(Locale.getDefault())
+        }
+    private var surname=surname
+        set(value){
+            field=if (value.isNotEmpty()) surname else throw Exception("Please enter a valid surname")
+        }
+        get() = field.toUpperCase(Locale.getDefault())
+
+    private var age=age
+        set(value) {
+            field=if (age>=18) value else throw Exception("You should older than 18 to use Bank")
+
+        }
+
+    fun depositMoney(amount:Int){
+        credit=credit+amount
+    }
+    fun withdrawMoney(amount:Int){
+        credit=credit-amount
+    }
+
+    fun showCreditInfo(){
+        println("Credit = $credit")
+    }
+    fun showMenu(){
+        println("To Deposit Enter 1")
+        println("To Withdraw Enter 2")
+        println("To Show Credit Info Enter 3")
+        print("Type here:")
+        val scanner:Scanner=Scanner(System.`in`)
+        var answer:Int=scanner.nextInt()
+        var amount=0
+
+        if (answer==1){
+            print("How much money do you want to deposit :")
+            amount=scanner.nextInt()
+            depositMoney(amount)
+        }else if (answer==2){
+            print("How much money to you want to withdraw :")
+            amount=scanner.nextInt()
+            withdrawMoney(amount)
+        }else if (answer==3){
+            showCreditInfo()
+        }
+        println()
+        showMenu()
+    }
+
 }
